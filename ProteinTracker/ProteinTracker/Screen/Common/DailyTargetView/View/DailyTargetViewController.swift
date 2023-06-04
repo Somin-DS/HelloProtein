9635538ce825c7daec0af1abafa1ad5f1691723d
Somin Park <ramgoods2@gmail.com>
2023-06-04 17:28:53 +0900

    [Add] 공통 - DailyTargetVC 추가

---
+++ ProteinTracker/ProteinTracker/Screen/Common/DailyTargetView/View/DailyTargetViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DailyTargetViewController.swift
+//  ProteinTracker
+//
+//  Created by Somin Park on 2023/06/04.
+//
+
+import UIKit
+
+enum DailyTargetType {
+    case onboarding, setting
+}
+class DailyTargetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
