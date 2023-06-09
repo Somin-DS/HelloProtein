832cc026682e73d9c220d6dc77caffffffa620de
Somin Park <ramgoods2@gmail.com>
2023-06-09 22:37:37 +0900

    [Chore] ActivateButton 초기화 구문 수정

--- ProteinTracker/ProteinTracker/Screen/Common/ActivateButton/ActivateButton.swift
+++ ProteinTracker/ProteinTracker/Screen/Common/ActivateButton/ActivateButton.swift
@@ -7,49 +7,56 @@
 
 import UIKit
 
-enum ButtonTypes {
+enum ActivateButtonType {
     case start
     case update
     case add
 }
 
 class ActivateButton: UIButton {
-
+    
     var isActive: Bool = false {
         didSet {
             self.isEnabled = isActive
-            if isActive {
-                self.backgroundColor = .pLBlue100
-            }else {
-                self.backgroundColor = .gray50
-            }
-            setNeedsLayout()
+            isActive ? (self.backgroundColor = .pLBlue100) : (self.backgroundColor = .gray50)
+            self.setNeedsLayout()
         }
     }
     
-    init(type: ButtonTypes) {
+    init() {
         super.init(frame: .zero)
-        self.backgroundColor = .gray50
-        self.setCornerRadius(cornerRadius: 24)
-        setButton(type)
+        configureButton()
     }
-    
-    private func setButton(_ type: ButtonTypes) {
-        var title: String
-        switch type {
-        case .start:
-            title = "Start"
-        case .update:
-            title = "Update"
-        case .add:
-            title = "Add"
-        }
-        self.setText(text: title, font: UIFont(name: "SFProRounded-Semibold", size: 20) ?? UIFont(), textColor: .white)
-    }
-    
+
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        configureButton()
     }
+    
+    private func configureButton() {
+        self.setCornerRadius(cornerRadius: 24)
+        self.backgroundColor = .gray50
+        self.snp.makeConstraints { make in
+            make.height.equalTo(44)
+        }
+        self.isEnabled = false
+    }
+    
+    
+    func setButton(_ type: ActivateButtonType) {
+           var title: String
+           switch type {
+           case .start:
+               title = "Start"
+           case .update:
+               title = "Update"
+           case .add:
+               title = "Add"
+           }
+        
+        //TODO - font extension
+        self.setButtonColorText(text: title, font: UIFont(name: "SFProRounded-Semibold", size: 20) ?? UIFont(), textColor: .white)
+       }
     
 }
 
