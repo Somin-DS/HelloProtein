009248bdf57e1cffc28ef014e36938b3a2dc491b
Somin Park <ramgoods2@gmail.com>
2023-06-28 22:51:22 +0900

    [Chore] DailyTargetVC: BaseVC적용 및 VM추가

--- ProteinTracker/ProteinTracker/Screen/Common/DailyTargetView/View/DailyTargetViewController.swift
+++ ProteinTracker/ProteinTracker/Screen/Common/DailyTargetView/View/DailyTargetViewController.swift
@@ -11,10 +11,10 @@ enum DailyTargetType {
     case onboarding, setting
 }
 
-final class DailyTargetViewController: UIViewController {
+final class DailyTargetViewController: BaseViewController<DailyTargetViewModel> {
 
-    @IBOutlet weak var weightTextField: FocusingTextField!
     @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var weightTextField: FocusingTextField!
     @IBOutlet weak var weightSegementControl: UISegmentedControl!
     @IBOutlet weak var activeLabel: UILabel!
     @IBOutlet weak var activeTableView: UITableView!
@@ -23,20 +23,23 @@ final class DailyTargetViewController: UIViewController {
     @IBOutlet weak var targetProteinLabel: UILabel!
     @IBOutlet weak var targetProteinTextField: FocusingTextField!
     @IBOutlet weak var targetDescLabel: UILabel!
-    
     private var activeButton = ActivateButton()
 
     private var type: DailyTargetType = .onboarding
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    }
-    override func viewWillAppear(_ animated: Bool) {
+        initViewModel(DailyTargetViewModel())
         configureButton()
-        weightTextField.configureKeyboard(.numberPad)
-        targetProteinTextField.configureKeyboard(.numberPad)
+        setupUI()
+        activeTableView.delegate = self
+        activeTableView.dataSource = self
+        optionalTableView.delegate = self
+        optionalTableView.dataSource = self
+        weightTextField.textField.delegate = self
+        targetProteinTextField.textField.delegate = self
     }
+    
     init?(type: DailyTargetType, corder: NSCoder) {
         super.init(coder: corder)
         self.type = type
@@ -44,6 +47,30 @@ final class DailyTargetViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func setupUI() {
+        view.backgroundColor = .secondarySystemBackground
+        weightLabel.setTitleText(viewModel?.weightTitle)
+        weightTextField.configureKeyboard(.numberPad)
+        weightSegementControl.setTitle(viewModel?.weightSegmentTitle[0], forSegmentAt: 0)
+        weightSegementControl.setTitle(viewModel?.weightSegmentTitle[1], forSegmentAt: 1)
+        
+        activeLabel.setTitleText(viewModel?.activeTitle)
+        activeTableView.setCornerRadius(cornerRadius: 10)
+        activeTableView.backgroundColor = .white
+        
+        optionalLabel.setTitleText(viewModel?.optionalTitle)
+        optionalTableView.setCornerRadius(cornerRadius: 10)
+        optionalTableView.backgroundColor = .white
+        
+        targetProteinLabel.setTitleText(viewModel?.targetProteinTitle)
+        targetProteinTextField.configureKeyboard(.numberPad)
+        
+        targetDescLabel.text = viewModel?.targetDescTitle
+        targetDescLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        targetDescLabel.textColor = .tertiaryLabel
     }
     
     private func configureButton() {
